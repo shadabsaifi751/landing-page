b363cf813f3e4571aa5ddac5715bafc5dda56357
shadab <shadabsaifi749@gmail.com>
2021-12-31 00:06:03 +0530

    add css

---
+++ src/components/data.js
@@ -0,0 +1,35 @@
+export const stockData = [
+    {
+      name: "Chair",
+      ticker: "TWTR",
+      stockPrice: "22.76 USD",
+      timeElapsed: "5 sec ago",
+    },
+    {
+      company: "table",
+      ticker: "SQ",
+      stockPrice: "45.28 USD",
+      timeElapsed: "10 sec ago",
+    },
+    {
+      company: "Mage",
+      ticker: "SHOP",
+      stockPrice: "341.79 USD",
+      timeElapsed: "3 sec ago",
+    },
+    {
+      company: "Sofa",
+      ticker: "RUN",
+      stockPrice: "9.87 USD",
+      timeElapsed: "4 sec ago",
+    },
+    {
+      company: "Bad",
+      ticker: "ADBE",
+      stockPrice: "300.99 USD",
+      timeElapsed: "10 sec ago",
+    }
+  ];
+
+
+  export default stockData;
--- src/components/product.js
+++ src/components/product.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import "./product.scss";
 import product1 from "./assect/p1.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import stockData from './data'
+
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination, Navigation } from "swiper";
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation]);
 
 const Product = () => {
+  const [clicked, setClicked] = useState(true);
+
+
+
+  const Toggle = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <section className="py-5 product">
       <Container>
@@ -14,55 +38,71 @@ const Product = () => {
           </Col>
         </Row>
         <Row>
-          <Col sm={12} md={4}>
-          <div class="feature-card">
-                {/* <img src={product1} className="img-fluid" alt="" /> */}
-                <div class="feature-card-tilte">
-                  <h3>Banking</h3>
-                </div>
-                <div class="feature-card-deatails">
-                  <i class="fa fa-bank"></i>
-                  <h4>Banking</h4>
-                  <p>
-                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-                    odit aut fugit
-                  </p>
-                  <a href="">Read More</a>
-                </div>
-              </div>
-          </Col>
-          <Col sm={12} md={4}>
-          <div class="feature-card">
-                <div class="feature-card-tilte">
-                  <h3>Banking</h3>
-                </div>
-                <div class="feature-card-deatails">
-                  <i class="fa fa-bank"></i>
-                  <h4>Banking</h4>
-                  <p>
-                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-                    odit aut fugit
-                  </p>
-                  <a href="">Read More</a>
-                </div>
-              </div>
-          </Col>
-          <Col sm={12} md={4}>
-          <div class="feature-card">
-                <div class="feature-card-tilte">
-                  <h3>Banking</h3>
-                </div>
-                <div class="feature-card-deatails">
-                  <i class="fa fa-bank"></i>
-                  <h4>Banking</h4>
-                  <p>
-                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-                    odit aut fugit
-                  </p>
-                  <a href="">Read More</a>
-                </div>
-              </div>
-          </Col>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            slidesPerGroup={3}
+            loop={true}
+            loopFillGroupWithBlank={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            className="mySwiper"
+          >
+            
+            
+              <SwiperSlide  >
+              {stockData.map((data) => 
+                  <Col sm={12} md={4}>
+                        <div className="wrapper">
+                          <div className="cont">
+                            <div className="top"></div>
+                            <div className={clicked ? "buy" : "bottom"}>
+                              <div className="left">
+                                <div className="details">
+                                  <h2>{data.name}</h2>
+                                  <p>£250</p>
+                                </div>
+                                <div className="buy" onClick={Toggle}>
+                                  <i class="bi bi-cart4"></i>
+                                </div>
+                              </div>
+                              <div className="right">
+                                <div className="done">
+                                  <i class="bi bi-check-square">done</i>
+                                </div>
+                                <div className="details">
+                                  <h2>Chair</h2>
+                                  <p>Added to your cart</p>
+                                </div>
+                                <div
+                                  className="remove"
+                                  onClick={Toggle}
+                                >
+                                  <i class="bi bi-arrow-left-circle">clear</i>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="inside">
+                            <div className="icon">
+                              <i class="bi bi-exclamation-square fs-5"></i>
+                            </div>
+                            <div className="contents">
+                              <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Et,
+                                cum!
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </Col>
+                  )}
+              </SwiperSlide>
+            
+          </Swiper>
+         
         </Row>
       </Container>
     </section>
